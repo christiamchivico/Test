2bbe3be33515511f70f28f53afd09591ec7db6be
cchivico <christiamchivico@gmail.com>
2016-10-03 15:02:36 -0430

    update component

--- web/js/controllers/ItemComponent.js
+++ web/js/controllers/ItemComponent.js
@@ -2,25 +2,28 @@ angular
     .module('Test')
     .component('itemComponent', {
 		controller	: 	'ItemController',
-		controllerAs: 	'items', 
-		template   	: 	['<table class="striped">',
-				            '<tr>',  
-				                '<td><b>Descripcion</b></td>',  
-				                '<td><b>Actividad</b></td>',  
-				                '<td><b>Encargado</b></td>',  
-				                '<td><b>Estatus</b></td>', 
-				                '<td><b>Accion</b></td>',  
-				            '</tr>',  
-				            '<tr ng-repeat="item in items.items">',  
-				                '<td>{{ item.descripcion }} </td>',  
-				                '<td>{{ item.actividad }} </td>',  
-				                '<td>{{ item.encargado }} </td>',  
-				                '<td>{{ item.estatus }} </td>',  
-				                '<td>',  
-				                    '<button ">Delete</button>',
-				                '</td>',  
-				            '</tr>',  
-				        '</table> '].join('')
+		template   	: 	[
+							'<table class="striped">',
+					            '<tr>',  
+					                '<td><b>Descripcion</b></td>',  
+					                '<td><b>Actividad</b></td>',  
+					                '<td><b>Encargado</b></td>',  
+					                '<td><b>Estatus</b></td>', 
+					                '<td><b>Accion</b></td>',  
+					            '</tr>',  
+					            '<tr ng-repeat="item in $ctrl.items">',  
+					                '<td>{{ item.descripcion }} </td>',  
+					                '<td>{{ item.actividad }} </td>',  
+					                '<td>{{ item.encargado }} </td>',  
+					                '<td>{{ item.estatus }} </td>',  
+					                '<td>',  
+					                    '<button ng-click="$ctrl.deleteItem(item)">Delete</button>',
+					                '</td>',  
+					            '</tr>',  
+					        '</table>' 
+				        ].join(''), 
+		bindings	: 	{ 'items' : '='}
+
 	});
 
  
--- web/js/controllers/ItemController.js
+++ web/js/controllers/ItemController.js
@@ -8,14 +8,15 @@ ItemController.$inject = [ 'ItemFactory' ];
 function ItemController( ItemFactory ){
     
     var vm = this;
-
-    vm.dataJson = ItemFactory.getData();
-    vm.items    = JSON.parse(ItemFactory.getItems());
     
+    vm.itemsInit   = JSON.parse(ItemFactory.getItems());
+    
+    if(vm.itemsInit == null){
+        vm.dataJson = ItemFactory.getData();
+    }
+
     vm.addItem  = function(data)
     {
-        alert('hola')
-        //vm.datos = JSON.stringify(vm.items)
         vm.items.push({
                 'descripcion'   : data.descripcion, 
                 'actividad'     : data.actividad, 
@@ -26,94 +27,11 @@ function ItemController( ItemFactory ){
 
     } 
 
-    angular.element(document).find('itemComponent').isolateScope().vm
-    console.log(vm.items)
+    vm.deleteItem = function ( item ) {  
+        vm.items.splice(vm.items.indexOf(item), 1);  
+        ItemFactory.saveData(vm.items);
+    }
 
+    vm.items    = JSON.parse(ItemFactory.getItems());
 
 }
-
-
-/*
-
-
-$scope.employees.push({ 'descripcion': $scope.descripcion, 'actividad': $scope.actividad, 'encargado': $scope.encargado, 'estatus': $scope.estatus  });  
-            getLocalStorage.updateEmployees($scope.employees);  
-            $scope.empno = '';  
-            $scope.empname = '';  
-            $scope.empsalary = '';  
-            $scope.count = $scope.employees.length;  
- 
-var isHtml5Compatible = document.createElement('canvas').getContext != undefined;  
-
-if (isHtml5Compatible) {  
-    initiateLocalStorage();  
-
-}  
-
-function initiateLocalStorage() {  
-    // Create the AngularJS app   
-    var app = angular.module('test', ['storageService']);  
-
-    // Create the Controller  
- 
-
-		getJsonData();
-		
-        //Read the Employee List from LocalStorage  
-        $scope.employees = getLocalStorage.getItems();  
-
-        //Count the Employee List  
-        $scope.count = $scope.employees.length;  
-
-
-        //Add Employee - using AngularJS push to add Employee in the Employee Object  
-        //Call Update Employee to update the locally stored Employee List  
-        //Reset the AngularJS Employee scope  
-        //Update the Count  
-        $scope.addEmployee = function () {  
-            $scope.employees.push({ 'descripcion': $scope.descripcion, 'actividad': $scope.actividad, 'encargado': $scope.encargado, 'estatus': $scope.estatus  });  
-            getLocalStorage.updateEmployees($scope.employees);  
-            $scope.empno = '';  
-            $scope.empname = '';  
-            $scope.empsalary = '';  
-            $scope.count = $scope.employees.length;  
-        };  
-
-        //Delete Employee - Using AngularJS splice to remove the emp row from the Employee list  
-        //All the Update Employee to update the locally stored Employee List  
-        //Update the Count  
-        $scope.deleteEmployee = function (emp) {  
-            $scope.employees.splice($scope.employees.indexOf(emp), 1);  
-            getLocalStorage.updateEmployees($scope.employees);  
-            $scope.count = $scope.employees.length;  
-        };  
-    }]);  
-
-    //Create the Storage Service Module  
-    //Create getLocalStorage service to access UpdateEmployees and getItems method  
-    var storageService = angular.module('storageService', []);  
-    storageService.factory('getLocalStorage', function ($http) {  
-        var employeeList = {};  
-        return {  
-            list: employeeList,  
-            updateEmployees: function (EmployeesArr) {  
-                if (window.localStorage && EmployeesArr) {  
-                    //Local Storage to add Data  
-                    localStorage.setItem("item", angular.toJson(EmployeesArr));  
-                }  
-                employeeList = EmployeesArr;  
-
-            },  
-            getItems: function () {  
-                //Get data from Local Storage  
-                employeeList = angular.fromJson(localStorage.getItem("item"));  
-                return employeeList ? employeeList : [];  
-            }
-            
-             
-        };  
-
-    });  
-}  
-
-*/
